7e24684886bd3cc00344342c4b7129a386de7658
blackpirateapps <github@blackpiratex.com>
2024-02-13 23:05:53 +0530

    updated color

--- script.js
+++ script.js
@@ -9,13 +9,8 @@ function generateQuoteAndColor() {
     // Set background color
     document.body.style.backgroundColor = randomColor;
 
-    // Adjust text color based on brightness of the background color
-    const brightness = calculateBrightness(randomColor);
-    const textColor = brightness < 128 ? '#333' : '#fff';
-    document.body.style.color = textColor;
-
     // Fetch quotes from the text file
-    fetch('quotes.txt')
+    fetch('quotes.txt') // Change 'quotes.txt' to the path of your quotes file
         .then(response => response.text())
         .then(data => {
             // Split the quotes by lines
@@ -28,23 +23,6 @@ function generateQuoteAndColor() {
         .catch(error => console.log(error));
 }
 
-// Function to calculate brightness of a color
-function calculateBrightness(color) {
-    const rgb = hexToRgb(color);
-    return (rgb.r * 299 + rgb.g * 587 + rgb.b * 114) / 1000;
-}
-
-// Function to convert hex color to RGB
-function hexToRgb(hex) {
-    const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-    hex = hex.replace(shorthandRegex, (m, r, g, b) => r + r + g + g + b + b);
-    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    return result ? {
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16)
-    } : null;
-}
-
 // Generate a quote and background color when the page loads
 generateQuoteAndColor();
+
